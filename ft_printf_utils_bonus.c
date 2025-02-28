e728ce7350389e019997b7b1d4a18c60f6b0d746
Ignacio Churruca Fernandez De Tejada <igchurru@c4r7s2.42urduliz.com>
2025-02-28 13:52:26 +0100

    Shitty approach. Restarting bonus.

--- ft_printf_utils_bonus.c
+++ ft_printf_utils_bonus.c
@@ -6,59 +6,9 @@
 /*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 10:58:46 by igchurru          #+#    #+#             */
-/*   Updated: 2025/02/28 13:28:03 by igchurru         ###   ########.fr       */
+/*   Updated: 2025/02/28 13:50:09 by igchurru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_bonus.h"
 
-void	ft_printstr(char *str, int *q, t_format *format)
-{
-	int	i;
-	int	len;
-	int	pad_len;
-
-	if (!str)
-		return (ft_printstr("(null)", q, format));
-	i = 0;
-	len = ft_strlen(str);
-	pad_len = format->width - len;
-	while (pad_len > 0 && !(format->flags & FLAG_MINUS))
-	{
-		*q += ft_printchar(' ', NULL);
-		pad_len--;
-	}
-	while (str[i])
-	{
-		*q += ft_printchar(str[i], NULL);
-		i++;
-	}
-	while (pad_len > 0 && format->flags & FLAG_MINUS)
-	{
-		*q += ft_printchar(' ', NULL);
-		pad_len--;
-	}
-}
-
-int	ft_printchar(char c, t_format *format)
-{
-	int	count;
-	int	padding;
-
-	if (!format)
-		return (write(1, &c, 1));
-	count = 0;
-	padding = format->width - 1;
-	while (padding > 0 && !(format->flags & FLAG_MINUS))
-	{
-		count += write(1, " ", 1);
-		padding--;
-	}
-	count += write(1, &c, 1);
-	while (padding > 0 && format->flags & FLAG_MINUS)
-	{
-		count += write(1, " ", 1);
-		padding--;
-	}
-	return (count);
-}
